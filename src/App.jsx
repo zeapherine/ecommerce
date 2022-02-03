9dc4b409d6d6ebecfc294acdedea6239694bb85e
zeapherine <zeapherine@gmail.com>
2022-02-03 19:45:52 +0530

    completed with the UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import Product from './pages/Product';
 import Home from './pages/Home';
 import ProductList from './pages/ProductList';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Cart from './pages/Cart';
 
 const App = () => {
-	return <ProductList />;
+	return <Cart />;
 };
 
 export default App;
